cdbbc797a30e7f37fc85ebcad62ef73d78d08fa9
Georgi <georgy.keranov@gmail.com>
2021-12-06 17:49:14 +0200

    Add required scripts for the bootstrap to work

--- views/parts/footer.php
+++ views/parts/footer.php
@@ -1,4 +1,6 @@
 	</main>
 
-	<script src="resources/js/bootstrap.bundle.min.js"></script>
+	<script src="resources/js/jquery-3.3.1.slim.min.js"></script>
+	<script src="resources/js/poper.min.js"></script>
+	<script src="resources/js/bootstrap.min.js"></script>
 </html>
--- views/parts/header.php
+++ views/parts/header.php
@@ -2,12 +2,34 @@
 <html>
 	<head>
 		<title>MVC Framework</title>
+		<meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="resources/css/bootstrap.min.css">
 	</head>
 
 	<body>
 		<header>
-			<h1>Header</h1>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<a class="navbar-brand" href="#">Navbar</a>
+				
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Features</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Pricing</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
 		</header>
 
 		<main>
