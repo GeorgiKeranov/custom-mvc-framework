a65c19a3ff9472b72bed97c769f4fafdbe9f91b7
Georgi <georgy.keranov@gmail.com>
2021-12-06 22:55:25 +0200

    Fix typo in "register.php" file

--- views/register.php
+++ views/register.php
@@ -5,7 +5,7 @@
 				<?php $is_username_error = !empty($params['errors']['username']); ?>
 
 				<label for="inputUsername">Username</label>
-				<input type="text" name="username" class="form-control<?php echo $is_username_error ? ' is-invalid' : ''?>" id="inputUsername" placeholder="Username" value="<?php echo !empty($params['username']) ? $params['username'] : '' ?>">
+				<input type="text" name="username" class="form-control<?php echo $is_username_error ? ' is-invalid' : '' ?>" id="inputUsername" placeholder="Username" value="<?php echo !empty($params['username']) ? $params['username'] : '' ?>">
 
 				<?php if ($is_username_error) : ?>
 					<div class="invalid-feedback">
@@ -18,7 +18,7 @@
 				<?php $is_password_error = !empty($params['errors']['password']); ?>
 
 				<label for="inputPassword4">Password</label>
-				<input type="password" name="password" class="form-control<?php echo $is_password_error ? ' is-invalid' : ''?>" id="inputPassword4" placeholder="Password">
+				<input type="password" name="password" class="form-control<?php echo $is_password_error ? ' is-invalid' : '' ?>" id="inputPassword4" placeholder="Password">
 
 				<?php if (!empty($params['errors']['password'])) : ?>
 					<div class="invalid-feedback">
@@ -32,7 +32,7 @@
 			<?php $is_email_error = !empty($params['errors']['email']); ?>
 
 			<label for="inputEmail4">Email</label>
-			<input type="email" name="email" class="form-control<?php echo $is_email_error ? ' is-invalid' : ''?>" id="inputEmail4" placeholder="Email"  value="<?php echo !empty($params['email']) ? $params['email'] : '' ?>">
+			<input type="email" name="email" class="form-control<?php echo $is_email_error ? ' is-invalid' : '' ?>" id="inputEmail4" placeholder="Email"  value="<?php echo !empty($params['email']) ? $params['email'] : '' ?>">
 
 			<?php if (!empty($params['errors']['email'])) : ?>
 				<div class="invalid-feedback">
