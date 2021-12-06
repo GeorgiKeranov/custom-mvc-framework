d2ba2678d7eff75f59a2604fc18687546209b36a
Georgi <georgy.keranov@gmail.com>
2021-12-06 18:02:34 +0200

    Add HOME_URL constant to the application

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
+
+// Define constants
 define('MAIN_DIRECTORY', __DIR__);
+define('HOME_URL', '//' . $_SERVER['HTTP_HOST'] . explode('/index.php', $_SERVER['SCRIPT_NAME'])[0] . '/');
 
 // Include autoload file from composer
 require_once(MAIN_DIRECTORY . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' );
