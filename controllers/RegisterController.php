29346dca8bff9870da7d31796ac080b47243b683
Georgi <georgy.keranov@gmail.com>
2021-12-06 18:44:27 +0200

    Add register controller and view

---
+++ controllers/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Controller;
+use app\models\UserModel;
+
+class RegisterController extends Controller {
+	public function get() {
+		$this->render('register');
+	}
+
+	public function post() {
+		$errors = UserModel::registerUser();
+
+		if ($errors) {
+			$this->render('register', $errors);
+		}
+
+		$this->redirectToPage('login?registered=true');
+	}
+}
---
+++ views/register.php
@@ -0,0 +1,22 @@
+<div class="container mt-5">
+	<form method="POST">
+		<div class="form-row">
+			<div class="form-group col-md-6">
+				<label for="inputUsername">Username</label>
+				<input type="text" name="username" class="form-control" id="inputUsername" placeholder="Username">
+			</div>
+
+			<div class="form-group col-md-6">
+				<label for="inputPassword4">Password</label>
+				<input type="password" name="password" class="form-control" id="inputPassword4" placeholder="Password">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="inputEmail4">Email</label>
+			<input type="email" name="email" class="form-control" id="inputEmail4" placeholder="Email">
+		</div>
+
+		<button type="submit" class="btn btn-primary">Sign in</button>
+	</form>
+</div><!-- /.register-form -->
