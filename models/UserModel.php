bc467caee7aa7fb10c50665bb9e608a86ce2b4fc
Georgi <georgy.keranov@gmail.com>
2021-12-06 21:20:51 +0200

    Add "UserModel" to handle user actions

--- models/UserModel.php
+++ models/UserModel.php
@@ -2,9 +2,57 @@
 
 namespace app\models;
 
-use app\core\Database;
+use app\core\Model;
 
-class UserModel
+class UserModel extends Model
 {
-	
+	public static function registerUser($fields) {
+		// Validate fields
+		$required_fields = ['username', 'password', 'email'];
+		$errors = self::validateFields($required_fields, $fields);
+
+		if ($errors) {
+			return $errors;
+		}
+
+		// Check if there is a user with the same username or email
+		$query = 'SELECT username, email FROM users WHERE username = ? OR email = ?';
+		$results = self::executeQuery($query, [$fields['username'], $fields['email']], 'ss');
+		if ($results->num_rows > 0) {
+			while ($row = $results->fetch_assoc()) {
+				$username = $row['username'];
+				$email = $row['email'];
+
+				if ($fields['username'] === $username) {
+					$errors['username'] = 'This username is already registered, please choose another one!';
+				}
+
+				if ($fields['email'] === $email) {
+					$errors['email'] = 'This email is already registered, please choose another one!';
+				}
+			}
+
+			return $errors;
+		}
+
+		// Encrypt the password
+		$fields['password'] = password_hash($fields['password'], PASSWORD_BCRYPT);
+
+		// Get current date
+		$current_date = date('Y-m-d');
+
+		// Generate indexed array of values in order to extract them
+		$fields_values = [
+			$fields['username'],
+			$fields['password'],
+			$fields['email'],
+			$current_date
+		];
+
+		// Register user
+		$query = 'INSERT INTO users (username, password, email, date_created) VALUES (?, ?, ?, ?)';
+		$results = self::executeQuery($query, $fields_values, 'ssss');
+
+		return false;
+	}
 }
