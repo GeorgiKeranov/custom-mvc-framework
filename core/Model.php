1eaf980c908b5c94c9434ad0a356bcfdf9a3cb11
Georgi <georgy.keranov@gmail.com>
2021-12-06 21:20:28 +0200

    Add core "Model" class

---
+++ core/Model.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\core;
+
+class Model
+{
+	public static function validateFields($requiredFields, $fields)
+	{
+		$errors = [];
+
+		foreach ($requiredFields as $requiredField) {
+			if (empty($fields[$requiredField])) {
+				$errors[$requiredField] = 'This field is required';
+			}
+		}
+
+		return $errors;
+	}
+
+	public static function executeQuery($query, $params, $paramsTypes)
+	{
+		$database = Database::getInstance();
+
+		return $database->executeQuery($query, $params, $paramsTypes);
+	}
+}
