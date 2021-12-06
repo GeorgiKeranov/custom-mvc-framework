5c46effa274cde318c01db6ca8c96e6c300bd9d0
Georgi <georgy.keranov@gmail.com>
2021-12-06 18:42:49 +0200

    Add "redirectToPage" method to the core Controller class

--- core/Controller.php
+++ core/Controller.php
@@ -4,28 +4,36 @@ namespace app\core;
 
 class Controller
 {
-	private function renderView($view, $render_error_view = true) {
+	private function renderView($view, $on_error_view = false, $params = []) {
 		$viewsFolder = MAIN_DIRECTORY . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
 		$viewPath = $viewsFolder . $view . '.php';
 
 		if (!file_exists($viewPath)) {
 			// Do not render nothing
-			if (!$render_error_view) {
+			if (!$on_error_view) {
 				return;
 			}
 
-			// View doesn't exists so return the views/404.php file
-			$viewPath = $viewsFolder . '404.php';
+			// View doesn't exists so return the on error view
+			$viewPath = $viewsFolder . $on_error_view;
 		}
 
+		// Include the view file
 		include_once($viewPath);
 	}
 
 	public function render($view, $params = []) {
 		$this->renderView('parts/header');
 
-		$this->renderView($view);
+		$this->renderView($view, '404', $params);
 
 		$this->renderView('parts/footer');
+	}
+
+	public function redirectToPage($page) {
+		$url = HOME_URL . $page;
+
+		header('Location: ' . $url);
+		exit();
 	}
 }
